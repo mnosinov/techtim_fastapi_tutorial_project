f0bbcf2c79a253815b2d2561bdd4fa95c7d6be60
mnosinov <mnosinov@gmail.com>
2025-07-29 13:42:54 +0500

    parameters

--- working.py
+++ working.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
 
@@ -17,7 +17,18 @@ inventory = {
 
 
 @app.get("/get-item/{item_id}")
-def get_item(item_id: int):
+def get_item(item_id: int = Path(
+    ..., description="The ID of the item you'd like to view", gt=0, lt=2)
+):
     return {
         "Data": inventory[item_id]
     }
+
+
+@app.get("/get-by-name")
+def get_item(name: str):
+    for item_id in inventory:
+        if inventory[item_id]["name"] == name:
+            return inventory[item_id]
+    return {"Data": "Not found"}
+
